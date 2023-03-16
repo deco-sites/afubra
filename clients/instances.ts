f9c4ce3a07fbd728c20d094d368ebac245fca709
lfroes <nikowluiz@gmail.com>
2023-03-16 11:51:25 -0300

    Feat: added lojasafubra to cart and catchall components

---
+++ clients/instances.ts
@@ -0,0 +1,18 @@
+import { createClient as createOccClient } from "deco-sites/std/commerce/occ/client.ts";
+// import { createClient as createShopifyClient } from "deco-sites/std/commerce/shopify/client.ts";
+import { createClient as createVTEXClient } from "deco-sites/std/commerce/vtex/client.ts";
+
+export const vtex = createVTEXClient({
+  account: "lojasafubra",
+  defaultLocale: "pt-BR",
+  defaultSalesChannel: "1",
+});
+
+// export const shopify = createShopifyClient({
+//   storeName: "gimenesdevstore",
+//   storefrontAccessToken: "27c1ac16fe30a0fb6c5d634eeb63bf81",
+// });
+
+export const occ = createOccClient({
+  baseUrl: "https://deco.cx.teste/",
+});
